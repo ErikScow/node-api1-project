bf8ee577b410a2aa0d67e173b529bd1eca75debd
Erik Scow <erikscow@gmail.com>
2020-07-09 17:52:13 -0700

    Added necessary packages for react app, built out component structure, and had first successful api call while implementing the cors middleware.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const shortid = require('shortid')
 
 
 const server = express()
 server.use(express.json())
+server.use(cors())
 
 let users = [
     {
--- react-app/stretch/src/App.js
+++ react-app/stretch/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
+import User from './components/User'
+import AddUserForm from './components/AddUserForm'
+
+
 
 function App() {
+
+  const [users, setUsers] = useState()
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/users`)
+          .then(res => {
+            setUsers(res.data.users)
+            console.log(res.data.users)
+          })
+          .catch(res => console.log('error retrieving from api:', res))
+  }, [])
+
   return (
     <div className="App">
+      <h1>List of Users</h1>
       
     </div>
   );
